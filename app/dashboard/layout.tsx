983dd836ba287a5f556e2fa8b3bce9c6b065d462
Faizan Ahmed <faizanzaid789@gmail.com>
2023-10-29 17:46:17 +0530

    progress: chapter 4 done

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
+
+// Layout file is shared across all pages and it will not re-render, only pages component will re-render.
