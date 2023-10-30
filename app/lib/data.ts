18c147236710d1d62892fe927509f06c04e74a56
Faizan Ahmed <faizanzaid789@gmail.com>
2023-10-30 17:26:01 +0530

    ui: added some more suspense boundaries

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -42,7 +42,7 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
